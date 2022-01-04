ddf53854eaa1f80a9340af34c371387d6113b6df
Camila Kunitz <camila.kunitz@gmail.com>
2022-01-04 09:24:56 -0300

    Semana 08 - Ex 02

---
+++ src/components/Resumo.js
@@ -0,0 +1,12 @@
+
+function Resumo() {
+  return (
+    <div>
+      <img src="https://github.com/camila-kunitz.png" alt="imagem de perfil"></img>
+      <h2>Dev Camila</h2>
+        
+    </div>
+  );
+}
+
+export default Resumo;
